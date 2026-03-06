4553c51c692b490f1d367867a77552f5ef685c6a
Irin <irin32613@gmail.com>
2026-03-06 15:55:14 +0700

    redeploy

--- app.py
+++ app.py
@@ -131,4 +131,4 @@ def history():
                          equipment_filter=equipment_filter)
 
 if __name__ == '__main__':
-    app.run(debug=True , port=5001)
+    app.run(host="0.0.0.0", port=5000)
